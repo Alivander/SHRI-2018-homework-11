1b8a38394a6c7e1b579f73ac6d489e37156e0813
Alina Vanieva <vanieva.alina@yandex.ru>
2018-04-06 03:21:29 +0300

    adds function saggestSort

--- source/js/saggest.js
+++ source/js/saggest.js
@@ -12,6 +12,8 @@
         }
     });
 
+    // функция saggest находит первые 10 названий, включающих подстроку
+
     function saggest(substr) {
         let time = performance.now();  // для определения скорости работы функции
 
@@ -40,5 +42,48 @@
 
         time = performance.now() - time;  // для определения скорости работы функции
         console.log('Время выполнения saggest = ', time, ' для строки ', substr);
+    }
+
+    // функция saggestSort находит 10 названий, включающих подстроку, с наилучшим результатом,
+    // т.е. те, где подстрока расположена ближе всего к началу строки.
+
+    function saggestSort(substr) {
+        let time = performance.now();  // для определения скорости работы функции
+
+        const fragment = document.createDocumentFragment();
+        const suitableStreets = [];
+
+        substr = substr.toLowerCase();
+
+        /* eslint-disable no-undef */
+        for (const street of streets) {
+        /* eslint-anaible no-undef */
+
+            const result = street.toLowerCase().includes(substr);
+            if (result) {
+                suitableStreets.push(street);
+            }
+        }
+
+        suitableStreets.sort((nameA, nameB) => {
+            const indexA = nameA.toLowerCase().indexOf(substr);
+            const indexB = nameB.toLowerCase().indexOf(substr);
+            if (indexA > indexB) {
+                return 1;
+            } else {
+                return -1;
+            }
+        });
+
+        for (let i = 0; i <= 10; i++) {
+            const item = document.createElement('li');
+            item.textContent = suitableStreets[i];
+            fragment.appendChild(item);
+        }
+
+        list.appendChild(fragment);
+
+        time = performance.now() - time;  // для определения скорости работы функции
+        console.log('Время выполнения saggestSort = ', time, ' для строки ', substr);
     }
 })();
