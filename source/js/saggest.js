a30b7aa506924ff7a15c4dee9f566e661657cb8c
Alina Vanieva <vanieva.alina@yandex.ru>
2018-04-06 19:41:14 +0300

    adds saggest function with clusters data

--- source/js/saggest.js
+++ source/js/saggest.js
@@ -3,7 +3,13 @@
 
     const saggestBlock = document.querySelector('.saggest');
     const input = saggestBlock.querySelector('.saggest__input');
-    const list = saggestBlock.querySelector('.saggest__list');
+    const listPlain = saggestBlock.querySelector('.saggest__list--plain');
+    const listClusters = saggestBlock.querySelector('.saggest__list--clusters');
+    const log = document.querySelector('.log');
+    const logForPlain = log.querySelector('.log__saggest--plain');
+    const logForClusters = log.querySelector('.log__saggest--clusters');
+
+    // Обрабатываем пользовательский ввод
 
     const delay = (function () {
         let timer = 0;
@@ -14,86 +20,128 @@
     })();
 
     input.addEventListener('input', () => {
-        list.innerHTML = '';
+        listPlain.innerHTML = '';
+        listClusters.innerHTML = '';
         if (input.value) {
             delay(() => {
                 saggest(input.value);
+                saggestClusters(input.value);
             }, 250);
         }
     });
 
+    // вспомогательная функция
+
+    function logging(message, log) {
+        const itemLog = document.createElement('li');
+        itemLog.textContent = message;
+        log.appendChild(itemLog);
+    }
+
     // функция saggest находит первые 10 названий, включающих подстроку
 
-    function saggest(substr) {
+      function saggest(substr) {
+          let time = performance.now();  // для определения скорости работы функции
+
+          const fragment = document.createDocumentFragment();
+          let counter = 0;
+
+          substr = substr.toLowerCase();
+
+          /* eslint-disable no-undef */
+          for (const street of streets) {
+          /* eslint-anaible no-undef */
+              if (counter >= 10) {
+                  break;
+              }
+
+              const result = street.toLowerCase().includes(substr);
+              if (result) {
+                  counter++;
+                  const item = document.createElement('li');
+                  item.textContent = street;
+                  fragment.appendChild(item);
+              }
+          }
+
+          listPlain.appendChild(fragment);
+
+          time = performance.now() - time;  // для определения скорости работы функции
+          logging(time, logForPlain);
+      }
+
+      // Пытаемся ускорить поиск - разбиваем массив данных на отдельные кластеры
+
+      /* eslint-disable no-undef */
+      /* отключаем no-undef в eslint для переменной массива данных - streets*/
+      const clusters = new Array(5);
+      const clusterValue = Math.floor(streets.length / clusters.length);
+
+      for (let i = 0; i < clusters.length - 1; i++) {
+          const j = clusterValue * i;
+          clusters[i] = streets.slice(j, j + clusterValue);
+      }
+
+      clusters[clusters.length - 1] = streets.slice(clusterValue * (clusters.length - 1));
+      /* eslint-anaible no-undef */
+
+    // функция saggestClusters находит первые 10 названий, включающих подстроку,
+    // перебирая отдельные кластеры из массива данных
+
+    function saggestClusters(substr) {
         let time = performance.now();  // для определения скорости работы функции
 
         const fragment = document.createDocumentFragment();
+        const results = [];
         let counter = 0;
 
         substr = substr.toLowerCase();
 
-        /* eslint-disable no-undef */
-        for (const street of streets) {
-        /* eslint-anaible no-undef */
-            if (counter >= 10) {
-                break;
-            }
+        function searchSubstr(substr, arrayStr) {
+            /* eslint-disable no-unused-vars */
+            return new Promise((resolve, reject) => {
+            /* eslint-anaible no-unused-vars */
 
-            const result = street.toLowerCase().includes(substr);
-            if (result) {
-                counter++;
-                const item = document.createElement('li');
-                item.textContent = street;
-                fragment.appendChild(item);
-            }
+                for (const str of arrayStr) {
+                    if (counter >= 10) {
+                        resolve(true);
+                        break;
+                    }
+
+                    const result = str.toLowerCase().includes(substr);
+                    if (result) {
+                        counter++;
+                        results.push(str);
+                    }
+                }
+                resolve(true);
+            });
         }
 
-        list.appendChild(fragment);
-
-        time = performance.now() - time;  // для определения скорости работы функции
-        console.log('Время выполнения saggest = ', time, ' для строки ', substr);
-    }
-
-    // функция saggestSort находит 10 названий, включающих подстроку, с наилучшим результатом,
-    // т.е. те, где подстрока расположена ближе всего к началу строки.
-
-    function saggestSort(substr) {
-        let time = performance.now();  // для определения скорости работы функции
-
-        const fragment = document.createDocumentFragment();
-        const suitableStreets = [];
-
-        substr = substr.toLowerCase();
-
-        /* eslint-disable no-undef */
-        for (const street of streets) {
-        /* eslint-anaible no-undef */
-
-            const result = street.toLowerCase().includes(substr);
-            if (result) {
-                suitableStreets.push(street);
+        // не успела переписать так, что бы промисы генерировались функцией
+        // и возвращались массивом с запущенными промисами
+        Promise.all([
+            searchSubstr(substr, clusters[0]),
+            searchSubstr(substr, clusters[1]),
+            searchSubstr(substr, clusters[2]),
+            searchSubstr(substr, clusters[3]),
+            searchSubstr(substr, clusters[4])
+        ]).then((ok) => {
+            if (ok) {
+                for (const result of results) {
+                    const item = document.createElement('li');
+                    item.textContent = result;
+                    fragment.appendChild(item);
+                }
+                listClusters.appendChild(fragment);
             }
-        }
-
-        suitableStreets.sort((nameA, nameB) => {
-            const indexA = nameA.toLowerCase().indexOf(substr);
-            const indexB = nameB.toLowerCase().indexOf(substr);
-            if (indexA > indexB) {
-                return 1;
-            } else {
-                return -1;
-            }
+        }).catch((error) => {
+            /* eslint-disable no-console */
+            console.log(`err: ${error}`);
+            /* eslint-anable no-console */
         });
 
-        for (let i = 0; i <= 10; i++) {
-            const item = document.createElement('li');
-            item.textContent = suitableStreets[i];
-            fragment.appendChild(item);
-        }
-
-        list.appendChild(fragment);
-
         time = performance.now() - time;  // для определения скорости работы функции
-        console.log('Время выполнения saggestSort = ', time, ' для строки ', substr);
+        logging(time, logForClusters);
     }
 })();
